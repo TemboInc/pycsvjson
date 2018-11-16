80ed1292eb68fcc34a7371565c3167af19311dba
Nicholas Tulach <nicholas.tulach@temboinc.com>
2018-11-16 14:08:46 -0500

    Updating version

--- pycsvjson/pycsvjson.py
+++ pycsvjson/pycsvjson.py
@@ -28,7 +28,7 @@ from jsonschema import RefResolver
 from jsonmapping import Mapper
 from tqdm import tqdm
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 def row_count(filename):
     """ Counts the rows in a given file """
